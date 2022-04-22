f3032d170b48d86a5c8eb59b1223acbbc612ba47
FeliciaPompei <f.pompei@hotmail.com>
2022-04-22 16:51:13 +0200

    styling card and added bonus: genre select

--- js/main.js
+++ js/main.js
@@ -2,12 +2,23 @@ const app = new Vue({
     'el' : '#app',
     data : {
         disks : [],
+        genre : []
+    },
+    methods : {
+        addGenre(){
+            
+        }
     },
     created(){
         axios.get('http://localhost/php-ajax-dischi/server/controller.php')
         .then((result) =>{
             this.disks = result.data.results;
             console.table(this.disks)
+            this.disks.forEach((discElement) => {
+                if (!this.genre.includes(discElement.genre)){
+                    this.genre.push(discElement.genre)
+            };
+            });
         })
         .catch((error)=>{
             console.warn(error);
