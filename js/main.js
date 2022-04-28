0ae7bdaa717867d7af2e65b184ca8d97644a04e0
FeliciaPompei <f.pompei@hotmail.com>
2022-04-28 18:45:20 +0200

    small change

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ const app = new Vue({
     'el' : '#app',
     data : {
         disks : [],
+        search : '',
     },
     created(){
-        axios.get(`http://localhost/php-ajax-dischi/server/controller.php?genre=`)
+        axios.get(`http://localhost/php-ajax-dischi/server/controller.php?genre=${this.search}`)
         .then((result) =>{
             this.disks = result.data.results;
             console.table(this.disks)
